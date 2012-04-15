7ba7d93880d305fe444711a5065afd9656d85ac0
Akira Kuroda <akuroda@gmail.com>
2012-04-15 21:24:20 +0900

    #2 update exmaple

--- example/jruby/MapReduceTestMain.rb
+++ example/jruby/MapReduceTestMain.rb
@@ -7,12 +7,12 @@ include Java
 # assume jars are located in lib directory
 require "lib/hadoop-common-0.23.0-cdh4b1.jar"
 require "lib/hadoop-mapreduce-client-core-0.23.0-cdh4b1.jar"
+require "lib/hadoop-jruby-adapter.jar"
 
 java_package 'com.akuroda.hadoop.jrubyexample'
 
-# need to create subclasses of MapperAdapter and ReducerAdapter in Java
-java_import 'com.akuroda.hadoop.jrubyexample.CountMapperAdapter'
-java_import 'com.akuroda.hadoop.jrubyexample.CountReducerAdapter'
+java_import 'com.akuroda.hadoop.MapperAdapter'
+java_import 'com.akuroda.hadoop.ReducerAdapter'
 
 java_import 'org.apache.hadoop.fs.Path'
 java_import 'org.apache.hadoop.io.IntWritable'
@@ -31,21 +31,21 @@ class MapReduceTestMain
     puts "MapReduceTest"
     job = Job.instance
    
-    # set MapperAdapter's subclass
-    job.mapperClass = CountMapperAdapter.java_class
+    # set actual mapper via hadoop-jruby-adapter-conf.xml
+    job.mapperClass = MapperAdapter.java_class
     puts job.mapperClass
     job.mapOutputKeyClass = Text.java_class
     puts job.mapOutputKeyClass
     job.mapOutputValueClass = IntWritable.java_class
     puts job.mapOutputValueClass
 
-    # set ReducerAdapter's subclass
-    job.reducerClass = CountReducerAdapter.java_class
-    puts job.getReducerClass
+    # set actual reducer via hadoop-jruby-adapter-conf.xml
+    job.reducerClass = ReducerAdapter.java_class
+    puts job.reducerClass
     job.outputKeyClass = Text.java_class
-    puts job.getOutputKeyClass
+    puts job.outputKeyClass
     job.outputValueClass = IntWritable.java_class
-    puts job.getOutputValueClass
+    puts job.outputValueClass
 
     FileInputFormat.addInputPath job, Path.new(args[0])
     FileOutputFormat.setOutputPath job, Path.new(args[1])
